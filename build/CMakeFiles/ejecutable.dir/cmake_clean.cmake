9567dcc7ec7894ae819b09b5d8317147ebf44969
anaguicas <angelicaaguicas@gmail.com>
2016-04-27 15:17:38 -0500

    archivos

---
+++ build/CMakeFiles/ejecutable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejecutable.dir/mostrarimagen.cc.o"
+  "ejecutable.pdb"
+  "ejecutable"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
